35574a3c5aa0c2d22c2c888db0f09ff4a975a2de
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-06-10 11:20:48 +0545

    Wireframe Script

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        const information = `${title} by ${author}, ${pages} pages, ${read}`;
+        return information; 
+    }
+}
+
+const theAlchemist = new Book("The Alchemist" , "Paulo Coelho", 183, "done reading");
+const thinketh = new Book("As a man thinketh", "James Allen" , 95, "not read yet");
+const nineTeenEightyFour = new Book("1984", "George Orwell" , 331, "not read yet");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien" , 295, "not read yet");
+
+console.log(theHobbit.info());
